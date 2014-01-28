45f449edea6e98ff86138c2704306b6b99a944f3
Hirofumi Katayama <katayama.hirofumi.mz@gmail.com>
2014-01-28 16:13:48 +0900

    v0.1

--- GC.cpp
+++ GC.cpp
@@ -1,4 +1,4 @@
-/* MZC3_GC -- MZC3 C/C++ garbage collection library
+/* MZC3_GC -- MZC3 C/C++ garbage collector library
    by Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
    This file is public domain software. */
 
